d264a4bba1c2b6cf96c024e24ccdc0d8843fe809
fishellVvv <victorvigilvela@gmail.com>
2025-10-02 19:23:44 +0200

    ajustes de formato

--- UT5_manejo_de_archivos_y_excepciones/ejercicioClaseArchivos/ejercicioClaseArchivos.py
+++ UT5_manejo_de_archivos_y_excepciones/ejercicioClaseArchivos/ejercicioClaseArchivos.py
@@ -1,7 +1,4 @@
-"""
-Programa que lea el archivo sistema_log_extenso.txt
-e imprima por pantalla solo los mensajes tipo ERROR
-"""
+# Programa que lea el archivo sistema_log_extenso.txt e imprima por pantalla solo los mensajes tipo ERROR
 
 log = open("./UT5_manejo_de_archivos_y_excepciones/ejercicioClaseArchivos/sistema_log_extenso.txt", "r")
 log_data = log.readlines()
--- UT5_manejo_de_archivos_y_excepciones/ejercicioClaseArchivos/ejercicioClaseArchivos2.py
+++ UT5_manejo_de_archivos_y_excepciones/ejercicioClaseArchivos/ejercicioClaseArchivos2.py
@@ -1,7 +1,4 @@
-"""
-Programa que lea el archivo sistema_log_extenso.txt
-y copie en otro archivo errores.txt solo los mensajes tipo ERROR
-"""
+# Programa que lea el archivo sistema_log_extenso.txt y copie en otro archivo errores.txt solo los mensajes tipo ERROR
 
 log = open("./UT5_manejo_de_archivos_y_excepciones/ejercicioClaseArchivos/sistema_log_extenso.txt", "r")
 log_errores = open("./UT5_manejo_de_archivos_y_excepciones/ejercicioClaseArchivos/errores.txt", "w")

e4ef9fa177b5aa0bd029e19d8d781459f1425551
fishellVvv <victorvigilvela@gmail.com>
2025-10-30 16:43:53 +0100

    ejemplo api

---
+++ UT7_conectividad/ejemplos_clase/ejemplo01.py
@@ -0,0 +1,8 @@
+import requests
+
+response = requests.get("https://pokeapi.co/api/v2/pokemon/ditto")
+
+if response.status_code == 200:
+    data = response.json()
+    for key, value in data.items():
+        print(key)

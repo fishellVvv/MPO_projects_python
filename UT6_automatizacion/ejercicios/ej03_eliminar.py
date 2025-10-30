be171db68a689b9e354922e550ce7190ce5d74d6
fishellVvv <victorvigilvela@gmail.com>
2025-10-30 10:09:02 +0100

    ej03

---
+++ UT6_automatizacion/ejercicios/ej03_eliminar.py
@@ -0,0 +1,18 @@
+import os
+
+def delete_file(path):
+    if not os.path.exists(path):
+        raise FileNotFoundError("El archivo especificado no existe")
+    if os.path.isdir(path):
+        raise ValueError(f"{path} es un directorio")
+    
+    os.remove(path)
+
+    return f"El archivo {path} a sido eliminado con éxito"
+    
+path = input("Indica la ruta del fichero que quieres eliminar:\n")
+
+try:
+    print(delete_file(path))
+except Exception as e:
+    print(e)

c99fcc01eb188e0821e1fc2d01e31825c685ace2
fishellVvv <victorvigilvela@gmail.com>
2025-11-21 19:26:06 +0100

    proyecto weather

---
+++ UT7_conectividad/proyecto_weather/weather_icons.py
@@ -0,0 +1,74 @@
+WEATHER_ICONS = {
+    "clear": r"""   
+    \___/    
+  __/   \__  
+    \   /    
+    /‾‾‾\    
+""",
+
+    "partly_cloudy": r"""
+   \__/      
+ --( _)-.    
+  ./(    ).  
+ (___.__)__) 
+""",
+
+    "cloudy": r"""
+     .--.    
+  .-(    ).  
+ (___.__)__) 
+             
+""",
+
+    "light_rain": r"""
+     .--.    
+  .-(    ).  
+ (___.__)__) 
+   '  '  '  
+""",
+
+    "heavy_rain": r"""
+     .--.    
+  .-(    ).  
+ (___.__)__) 
+  | | | | |  
+""",
+
+    "storm": r"""
+     .--.    
+  .-(    ).  
+ (___.__)__) 
+   / Z / Z   
+""",
+
+    "snow": r"""
+     .--.    
+  .-(    ).  
+ (___.__)__) 
+   *  *  *   
+""",
+
+    "fog": r"""
+  ~ ~ ~ ~ ~  
+ ~ ~ ~ ~ ~ ~ 
+  ~ ~ ~ ~ ~  
+ ~ ~ ~ ~ ~ ~ 
+""",
+
+    "windy": r"""
+    __       
+  .(  ).~)~) 
+ (___)__))~) 
+             
+"""
+}
+
+print(WEATHER_ICONS["clear"])
+print(WEATHER_ICONS["partly_cloudy"])
+print(WEATHER_ICONS["cloudy"])
+print(WEATHER_ICONS["light_rain"])
+print(WEATHER_ICONS["heavy_rain"])
+print(WEATHER_ICONS["storm"])
+print(WEATHER_ICONS["snow"])
+print(WEATHER_ICONS["fog"])
+print(WEATHER_ICONS["windy"])
